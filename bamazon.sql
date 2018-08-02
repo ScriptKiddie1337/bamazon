b9af117c2312b622a272c330985be71694e0d19b
Script Kiddie <37413338+ScriptKiddie1337@users.noreply.github.com>
2018-08-02 07:56:46 -0700

    framework

---
+++ bamazon.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE database bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products (
+  position INT NOT NULL,
+  item_id
+  product_name
+  department_name
+  price
+  stock_quantity
+  PRIMARY KEY (position)
+);
